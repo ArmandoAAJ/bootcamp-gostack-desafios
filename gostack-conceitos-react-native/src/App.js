d2055e2c0a842c6d8b665b53c13a3a84d85533da
Armando Arlan Joergensen <armando-a-j@hotmail.com>
2021-03-01 17:16:43 -0300

    Create
    Gostack conceitos react native

---
+++ gostack-conceitos-react-native/src/App.js
@@ -0,0 +1,126 @@
+import React, { useEffect, useState } from "react";
+
+import api from './services/api';
+import {
+  SafeAreaView,
+  View,
+  FlatList,
+  Text,
+  StatusBar,
+  StyleSheet,
+  TouchableOpacity,
+} from "react-native";
+
+export default function App() {
+  const [repository, setRepository] = useState([]);
+
+  useEffect(() => {
+    async function loadRepo() {
+      const response = await api.get('/repositories');
+      setRepository(response.data);
+    }
+    loadRepo();
+  }, []);
+
+  async function handleLikeRepository(id) {
+      const response = await api.post(`/repositories/${id}/like`);
+      const repositoryIndex = repository.findIndex(repository => repository.id === id);
+      repository[repositoryIndex] = response.data;
+      setRepository([...repository]);
+  }
+
+  return (
+    <>
+      <StatusBar barStyle="light-content" backgroundColor="#7159c1" />
+      <SafeAreaView style={styles.container}>
+        <FlatList
+          data={repository}
+          keyExtractor={item => item.id}
+          renderItem={({item}) => (
+            <View style={styles.repositoryContainer}>
+              <Text style={styles.repository}>{item.id}</Text>
+
+              <View style={styles.techsContainer}>
+                {item.techs.map(i => (
+                  <Text key={i} style={styles.tech}>
+                    {i}
+                  </Text>
+                ))}
+              </View>
+
+              <View style={styles.likesContainer}>
+                <Text
+                  style={styles.likeText}
+                  // Remember to replace "1" below with repository ID: {`repository-likes-${repository.id}`}
+                  testID={`repository-likes-${item.id}`}
+                >
+                  {item.likes} curtidas
+            </Text>
+              </View>
+
+              <TouchableOpacity
+                style={styles.button}
+                onPress={() => handleLikeRepository(item.id)}
+                // Remember to replace "1" below with repository ID: {`like-button-${repository.id}`}
+                testID={`like-button-${item.id}`}
+              >
+                <Text style={styles.buttonText}>Curtir</Text>
+              </TouchableOpacity>
+            </View>
+          )}
+        />
+      </SafeAreaView>
+    </>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#7159c1",
+  },
+  repositoryContainer: {
+    marginBottom: 15,
+    marginHorizontal: 15,
+    backgroundColor: "#fff",
+    padding: 20,
+  },
+  repository: {
+    fontSize: 10,
+    fontWeight: "bold",
+  },
+  techsContainer: {
+    flexDirection: "row",
+    marginTop: 10,
+  },
+  tech: {
+    fontSize: 12,
+    fontWeight: "bold",
+    marginRight: 10,
+    backgroundColor: "#04d361",
+    paddingHorizontal: 10,
+    paddingVertical: 5,
+    color: "#fff",
+  },
+  likesContainer: {
+    marginTop: 15,
+    flexDirection: "row",
+    justifyContent: "space-between",
+  },
+  likeText: {
+    fontSize: 14,
+    fontWeight: "bold",
+    marginRight: 10,
+  },
+  button: {
+    marginTop: 10,
+  },
+  buttonText: {
+    fontSize: 14,
+    fontWeight: "bold",
+    marginRight: 10,
+    color: "#fff",
+    backgroundColor: "#7159c1",
+    padding: 15,
+  },
+});
